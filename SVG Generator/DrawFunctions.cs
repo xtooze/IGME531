4892a8980ec48f755b5cb32234b98f9f24d27d26
xtooze <xandertooze@gmail.com>
2024-01-23 15:03:17 -0500

    Created an SVG generator that can make shapes using rects, circles, ellipses, lines, and polygons

---
+++ SVG Generator/DrawFunctions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SVGGenerator
+{
+    public class DrawFunctions
+    {
+        public string drawRect(string stroke, string fill, double x, double y, double width, double height, double angle)
+        {
+            return $"<rect stroke=\"{stroke}\" fill=\"{fill}\" x=\"{x}\" y=\"{y}\" width=\"{width}\" height=\"{height}\" transform=\"rotate({angle},{x + width / 2},{y + height / 2})\"></rect>";
+        }
+
+        public string drawCircle(string stroke, string fill, double cx, double cy, double r)
+        {
+            return $"<circle stroke=\"{stroke}\" fill=\"{fill}\" cx=\"{cx}\" cy=\"{cy}\" r=\"{r}\"></circle>";
+        }
+
+        public string drawEllipse(string stroke, string fill, double cx, double cy, double rx, double ry, double angle)
+        {
+            return $"<ellipse stroke=\"{stroke}\" fill=\"{fill}\" cx=\"{cx}\" cy=\"{cy}\" rx=\"{rx}\" ry=\"{rx}\" transform=\"rotate({angle},{cx + rx / 2},{cy + ry / 2})\"></ellipse>";
+        }
+
+        public string drawLine(string stroke, double x1, double y1, double x2, double y2)
+        {
+            return $"<line stroke=\"{stroke}\" x1=\"{x1}\" y1=\"{y1}\" x2=\"{x2}\" y2=\"{y2}\"></line>";
+        }
+
+        public string drawPolygon(string stroke, string fill, double[] points)
+        {
+            string pointsString = "";
+            for (int i = 0; i < points.Length; i++)
+            {
+                pointsString += points[i].ToString();
+                if (i % 2 == 0)
+                {
+                    pointsString += ",";
+                }
+                else
+                {
+                    pointsString += " ";
+                }
+            }
+            return $"<polygon stroke=\"{stroke}\" fill=\"{fill}\" points=\"{pointsString}\"></polygon>";
+        }
+
+        public DrawFunctions()
+        {
+        }
+    }
+}
---
+++ SVG Generator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using SVGGenerator;
+
+//WriteSVG(new DrawFunctions(), "../../../Sainte-Victoire en Rouge.svg");
+WriteSVG(new DrawFunctions(), "../../../Sainte-Victoire en Rouge Circles.svg");
+
+
+void WriteSVG(DrawFunctions df, string path)
+{
+    StreamWriter sw = File.CreateText(path);
+    try
+    {
+        sw.WriteLine("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" viewBox=\"0 0 100 100\">");
+
+        //sw.WriteLine(df.drawRect("#BE2A2A", "#BE2A2A", 1, 35, 20, 20, 5));
+        //sw.WriteLine(df.drawRect("#6B2933", "#6B2933", 12, 7, 20, 20, -20));
+        //sw.WriteLine(df.drawRect("#BE2A2A", "#BE2A2A", 25, 7, 20, 20, -5));
+        //sw.WriteLine(df.drawRect("#6B2933", "#6B2933", 54, 42, 20, 20, 5));
+        //sw.WriteLine(df.drawRect("#6B2933", "#6B2933", 43, 43, 20, 20, -20));
+        //sw.WriteLine(df.drawRect("#BE2A2A", "#BE2A2A", 55, 58, 20, 20, 43));
+        //sw.WriteLine(df.drawRect("#E52523", "#E52523", 65, 60, 20, 20, 20));
+
+        //sw.WriteLine(df.drawRect("#E84139", "#E84139", 12, 20, 20, 20, 43));
+        //sw.WriteLine(df.drawRect("#E84139", "#E84139", 40, 23, 20, 20, -10));
+
+        sw.WriteLine(df.drawCircle("#BE2A2A", "#BE2A2A", 11, 45, 12));
+        sw.WriteLine(df.drawCircle("#6B2933", "#6B2933", 22, 17, 12));
+        sw.WriteLine(df.drawCircle("#BE2A2A", "#BE2A2A", 35, 17, 12));
+        sw.WriteLine(df.drawCircle("#6B2933", "#6B2933", 64, 52, 12));
+        sw.WriteLine(df.drawCircle("#6B2933", "#6B2933", 53, 53, 12));
+        sw.WriteLine(df.drawCircle("#BE2A2A", "#BE2A2A", 65, 68, 12));
+        sw.WriteLine(df.drawCircle("#E52523", "#E52523", 75, 70, 12));
+
+        sw.WriteLine(df.drawCircle("#E84139", "#E84139", 22, 30, 12));
+        sw.WriteLine(df.drawCircle("#E84139", "#E84139", 50, 33, 12));
+
+        sw.WriteLine("</svg>");
+    }
+    catch (Exception e)
+	{
+        Console.WriteLine(e.Message);
+	}
+	finally 
+	{
+        sw.Close();
+    }
+}
