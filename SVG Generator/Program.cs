c3502b29652905a4cd692796a139268c653509a8
xat7714 <xat7714@rit.edu>
2024-02-20 12:05:11 -0500

    made tile generation

--- SVG Generator/Program.cs
+++ SVG Generator/Program.cs
@@ -6,7 +6,7 @@ using SVGGenerator;
 Random rng = new Random();
 FastNoiseLite noise = new FastNoiseLite();
 float[,] noiseNums = noiseSetUp(noise, 100, 100, .05f);
-WriteSVG(new DrawFunctions(), "../../../InteruptionsUniformLean.svg");
+WriteSVG(new DrawFunctions(), "../../../TileTestNoise.svg");
 
 void WriteSVG(DrawFunctions df, string path)
 {
@@ -68,17 +68,19 @@ void WriteSVG(DrawFunctions df, string path)
         //}
 
         //Interuptions - Uniform Lean
-        for (int i = 0; i < 100; i++)
-        {
-            for (int j = 0; j < 100; j++)
-            {
-                if (noiseNums[i, j] > .3)
-                {
-                    continue;
-                }
-                sw.WriteLine(df.rotate(df.drawLine($"rgb({0},{0},{0})", i, j, i + 2, j), 360 * noiseNums[i, j], (i + i + 2) / 2, j));
-            }
-        }
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    for (int j = 0; j < 100; j++)
+        //    {
+        //        if (noiseNums[i, j] > .3)
+        //        {
+        //            continue;
+        //        }
+        //        sw.WriteLine(df.rotate(df.drawLine($"rgb({0},{0},{0})", i, j, i + 2, j), 360 * noiseNums[i, j], (i + i + 2) / 2, j));
+        //    }
+        //}
+
+        sw.WriteLine(drawTile(df,4,4,4));
 
         sw.WriteLine("</svg>");
     }
@@ -217,4 +219,86 @@ float[,] noiseSetUp(FastNoiseLite noise, int x, int y, float f)
     }
 
     return noiseData;
+}
+
+string tileCurve(DrawFunctions df, double x, double y, double width, double height, int upperNum, int lowerNum)
+{
+    string tile = "";
+    for (int i = 2; i < upperNum + 2; i++)
+    {
+        if (i==2)
+        {
+            tile += df.drawLine("black", x, y + height / i, x + width / i, y);
+        }
+        else
+        {
+            tile += df.drawLine("red", x, y + height / i, x + width / i, y);
+        }
+    }
+    for (int i = 2; i < lowerNum + 2; i++)
+    {
+        if (i==2)
+        {
+            tile += df.drawLine("black", x + width / i, y + height, x + width, y + height / i);
+        }
+        else
+        {
+            tile += df.drawLine("red", x + width / i, y + height, x + width, y + height / i);
+        }
+    }
+    return tile;
+}
+
+string drawTile(DrawFunctions df, double tileWidth, double tileHeight, int boundMax)
+{
+    string tileGrid = "";
+    for (double i = 0; i < 100; i += tileWidth)
+    {
+        for (double j = 0; j < 100; j += tileHeight)
+        {
+            switch (rng.Next(0, 4))
+            {
+                case 0:
+                    tileGrid += tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax));
+                    break;
+                case 1:
+                    tileGrid += df.rotate(tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax)), 90, i + tileWidth / 2, j + tileHeight / 2);
+                    break;
+                case 2:
+                    tileGrid += df.rotate(tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax)), 180, i + tileWidth / 2, j + tileHeight / 2);
+                    break;
+                case 3:
+                    tileGrid += df.rotate(tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax)), 270, i + tileWidth / 2, j + tileHeight / 2);
+                    break;
+            }
+        }
+    }
+    return tileGrid;
+}
+
+string drawTileNoise(DrawFunctions df, double tileWidth, double tileHeight, int boundMax, float[,] noiseGrid)
+{
+    string tileGrid = "";
+    for (double i = 0; i < 100; i += tileWidth)
+    {
+        for (double j = 0; j < 100; j += tileHeight)
+        {
+            switch (rng.Next(0, 4))
+            {
+                case 0:
+                    tileGrid += tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax));
+                    break;
+                case 1:
+                    tileGrid += df.rotate(tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax)), 90, i + tileWidth / 2, j + tileHeight / 2);
+                    break;
+                case 2:
+                    tileGrid += df.rotate(tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax)), 180, i + tileWidth / 2, j + tileHeight / 2);
+                    break;
+                case 3:
+                    tileGrid += df.rotate(tileCurve(df, i, j, tileWidth, tileHeight, rng.Next(0, boundMax), rng.Next(0, boundMax)), 270, i + tileWidth / 2, j + tileHeight / 2);
+                    break;
+            }
+        }
+    }
+    return tileGrid;
 }
